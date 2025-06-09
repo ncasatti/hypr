36731705b14bca4dada85ffdd4e26d35bef639a6
Nicolas Casatti <ncasatti@gmail.com>
2025-06-09 12:43:05 -0300

    4.6

--- .configs/hyprshade/shaders/blue-light-filter.glsl
+++ .configs/hyprshade/shaders/blue-light-filter.glsl
@@ -4,7 +4,7 @@ precision highp float;
 varying vec2 v_texcoord;
 uniform sampler2D tex;
 
-const float temperature = 3500.0;
+const float temperature = 5000.0;
 const float temperatureStrength = 1.0;
 
 #define WithQuickAndDirtyLuminancePreservation
@@ -21,7 +21,7 @@ vec3 colorTemperatureToRGB(const in float temperature) {
                                             vec3(-2666.3474220535695, -2173.1012343082230, 2575.2827530017594),
                                             vec3(0.55995389139931482, 0.70381203140554553, 1.8993753891711275));
     return mix(clamp(vec3(m[0] / (vec3(clamp(temperature, 1000.0, 40000.0)) + m[1]) + m[2]), vec3(0.0), vec3(1.0)),
-               vec3(1.0), smoothstep(1000.0, 0.0, temperature));
+              vec3(1.0), smoothstep(1000.0, 0.0, temperature));
 }
 
 void main() {
@@ -32,7 +32,7 @@ void main() {
 
 #ifdef WithQuickAndDirtyLuminancePreservation
     color *= mix(1.0, dot(color, vec3(0.2126, 0.7152, 0.0722)) / max(dot(color, vec3(0.2126, 0.7152, 0.0722)), 1e-5),
-                 LuminancePreservationFactor);
+                LuminancePreservationFactor);
 #endif
 
     color = mix(color, color * colorTemperatureToRGB(temperature), temperatureStrength);
